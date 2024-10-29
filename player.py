148406c86febb642343c66aa50c7d393925f10f9
Michael Sorensen <sorensenmichael@meta.com>
2024-10-29 15:30:18 -0700

    initial attempts at making a GUI interface that allows me to use a standard (not insane) layout

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 from circleshape import CircleShape
 from constants import *
 
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -17,10 +18,10 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-    
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
